d8fb76edacb849140b360f92bcc890d34d6f7a99
ben <b.talbot@qut.edu.au>
2021-03-16 08:07:26 +1000

    Majorly refactor package structure (& break repo)

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(name='refinenet',
+      version='0.1.0',
+      author='Ben Talbot',
+      author_email='b.talbot@qut.edu.au',
+      description='Refinenet semantic segmentation',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      packages=find_packages(),
+      install_requires=[],
+      classifiers=(
+          "Programming Language :: Python :: 3",
+          "License :: OSI Approved :: BSD License",
+          "Operating System :: OS Independent",
+      ))
