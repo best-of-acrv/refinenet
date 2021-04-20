8dc96336650b1fb0491fb2435cdb89561657216e
Ben Talbot <b.talbot@qut.edu.au>
2021-04-21 09:13:25 +1000

    Add parent parser for shared arguments

--- refinenet/__main__.py
+++ refinenet/__main__.py
@@ -4,7 +4,8 @@ import sys
 import textwrap
 
 
-class ShowNewlines(argparse.RawDescriptionHelpFormatter):
+class ShowNewlines(argparse.ArgumentDefaultsHelpFormatter,
+                   argparse.RawDescriptionHelpFormatter):
 
     def _fill_text(self, text, width, indent):
         return ''.join([
@@ -20,6 +21,7 @@ def main():
     # Parse command line arguments
     w = shutil.get_terminal_size().columns
     p = argparse.ArgumentParser(
+        prog='refinenet',
         formatter_class=ShowNewlines,
         description="RefineNet semantic image segmentation.\n\n"
         "Dataset interaction is performed through the acrv_datasets package. "
@@ -27,27 +29,45 @@ def main():
         "and changing where they are stored.\n\n"
         "For full documentation of RefineNet, please see "
         "https://github.com/best-of-acrv/refinenet.")
-    sp = p.add_subparsers(help="RefineNet modes of operation")
+
+    p_parent = argparse.ArgumentParser(add_help=False)
+    p_parent.add_argument('--gpu-id',
+                          default=0,
+                          help="ID of GPU to use for model")
+    sp = p.add_subparsers()
 
     p_eval = sp.add_parser(
         'evaluate',
+        parents=[p_parent],
+        formatter_class=ShowNewlines,
         help="Evaluate a model's performance against a specific dataset")
     p_eval.add_argument(
         'dataset_name',
-        help="Name of the dataset to use (see "
-        "https://github.com/best-of-acrv/acrv-datasets for valid names)")
+        help="Name of the dataset to use (run "
+        "'acrv_datasets --supported-datasets' to see valid names)")
     p_eval.add_argument(
         '--dataset-dir',
         default=None,
-        help=
-        "Custom directory to search for datasets instead of 'acrv_datasets' current default"
-    )
+        help="Search this directory for datasets instead of the current "
+        "default in 'acrv_datasets'")
+    p_eval.add_argument(
+        '--multi-scale',
+        default=False,
+        help="Generate predictions using multiple image scales")
+    p_eval.add_argument('--output-directory',
+                        default='.',
+                        help="Directory to save evaluation results")
+    p_eval.add_argument('--output-images',
+                        default=False,
+                        help="Save prediction images as part of the results")
 
     p_pred = sp.add_parser('predict',
+                           formatter_class=ShowNewlines,
                            help="Use a model to predict image segmentation "
                            "from a given input image")
 
     p_train = sp.add_parser('train',
+                            formatter_class=ShowNewlines,
                             help="Train a model from a previous starting "
                             "point using a specific dataset")
 
