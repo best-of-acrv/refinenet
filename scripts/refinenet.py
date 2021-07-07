be15dc9cdd44a119c63674ffeceb9b31eef19146
Ben Talbot <b.talbot@qut.edu.au>
2021-07-07 11:55:37 +1000

    Remove unnecessary scripts directory

--- scripts/refinenet.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-import os
-import sys
-sys.path.insert(
-    0, os.path.abspath(os.path.join(os.path.dirname(sys.argv[0]), '..')))
-from refinenet import run_from_args
-
-run_from_args()
